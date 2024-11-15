056956ff3ad36a8cd452c21bb6a54956366331fc
Singul0 <127663818+Singul0@users.noreply.github.com>
2024-11-16 06:19:06 +0700

    html done

---
+++ number_guesser_game copy/script.js
@@ -0,0 +1,4 @@
+const palindrome_text = document.querySelector(".text_submitted")
+const submit_palindrome = document.querySelector(".submit_text")
+const low_or_high = document.querySelector(".answer")
+let
