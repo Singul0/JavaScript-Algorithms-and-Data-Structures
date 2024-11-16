f7ecb883ad5edc93d68e3812af32dd7eca8bd5ab
Singul0 <127663818+Singul0@users.noreply.github.com>
2024-11-16 15:35:51 +0700

    minimum viable website done

--- number_guesser_game copy/script.js
+++
@@ -1,4 +0,0 @@
-const palindrome_text = document.querySelector(".text_submitted")
-const submit_palindrome = document.querySelector(".submit_text")
-const low_or_high = document.querySelector(".answer")
-let
