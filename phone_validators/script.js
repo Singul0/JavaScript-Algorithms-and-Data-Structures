8085ee7fee1d12de36696246188fe44d093b04ca
Singul0 <127663818+Singul0@users.noreply.github.com>
2024-12-26 12:30:59 +0700

    frankly I do not want to deal with regexes, so i'll just nip sari's regex :)

--- phone_validators/script.js
+++ phone_validators/script.js
@@ -2,6 +2,7 @@ const number_field = document.querySelector("#user-input");
 const submit_int = document.querySelector("#check-btn");
 const clear_result = document.querySelector("#clear-btn");
 const results = document.querySelector("#results-div");
+const phone_regex = /(?:^|\D)\(([2-9])(?:\d(?!\1)\d|(?!\1)\d\d)\)\s*[2-9]\d{2}-\d{4}/;
 
 //Clear button
 clear_result.addEventListener('click', () => {
@@ -19,11 +20,24 @@ submit_int.addEventListener('click', () => {
     number_field.value = '';
 });
 
-function check_number() {
-    const phone_input = number_field.value;
+// Regexes untuk validasi US phone numbers, dikumpulin dalam satu function (thanks sarixfauzah :) )
+const createPhoneNumberRegex = () => {
+    const countryCode = '^(1\\s?)?';
+    const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+    const spacesDashes = '[\\s\\-]?';
+    const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
 
+    return new RegExp(`${countryCode}${areaCode}${spacesDashes}${phoneNumber}`);
+};
+
+function check_number() {
+    const phone_input = String(number_field.value);
+    const phone_regex = createPhoneNumberRegex();
     if(phone_input === ""){
         alert("Please provide a phone number");
-        return;
+        return false;
     }
+    const is_valid = phone_regex.test(phone_input);
+
+    results.innerHTML += `${is_valid ? "Valid" : "Invalid"} US number: ${phone_input}`;
 }
